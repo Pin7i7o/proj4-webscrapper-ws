b21fcf953b9301c25a872542b7a9c09039775af7
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2024-05-02 22:10:59 +0100

    updated index.js
    Saves each car as an object and then to the array.
    Now gets the 10 most recent cars.

--- index.js
+++ index.js
@@ -4,22 +4,43 @@ const fs = require('fs');
 
 const url = 'https://www.standvirtual.com/carros?search%5Bfilter_enum_fuel_type%5D=electric&search%5Border%5D=created_at_first%3Adesc';
 
-const electric_cars = {};
+const first_10_cars = [];
 
 async function getHtml() {
-    const { data: html } = await axios.get(url);
-    return html;
+    try {
+        const { data: html } = await axios.get(url);
+        return html;
+    } catch (error) {
+        throw error;
+    }
 }
 
-getHtml().then((res) => {
-    const $ = cheerio.load(res);
-    $('[data-testid="search-results"] section.ooa-10gfd0w.e1i3khom1').each((i, car) => {
-        const make_model = $(car).find('h1.e1i3khom9.ooa-1ed90th.er34gjf0 > a').text();
-        const km = $(car).find('[data-parameter="mileage"]').text();
-        electric_cars[make_model] = km;
-    });
-    fs.writeFile('cars.json', JSON.stringify(electric_cars), (err) => {
-        if (err) throw err;
-        console.log('file successfully saved');
+getHtml()
+    .then((res) => {
+        const $ = cheerio.load(res);
+        const cars = $('[data-testid="search-results"] section.ooa-10gfd0w.e1i3khom1');
+        let count = 0;
+        cars.each((i, car) => {
+            if (count == 10) return;
+            const data = {
+                model_make: $(car).find('h1.e1i3khom9.ooa-1ed90th.er34gjf0 > a').text(),
+                km: $(car).find('[data-parameter="mileage"]').text(),
+                year: $(car).find('[data-parameter="first_registration_year"]').text(),
+                price: $(car).find('h3.e1i3khom16.ooa-1n2paoq.er34gjf0').text(),
+                url: $(car).find('h1.e1i3khom9.ooa-1ed90th.er34gjf0 > a').attr('href')
+            };
+
+            first_10_cars.push(data);
+            count++;
+        });
+
+        const jsonData = JSON.stringify(first_10_cars);
+        fs.writeFile('cars.json', jsonData, (err) => {
+            if (err) throw err;
+            console.log('Data written to file');
+        });
     })
-});
+    .catch((error) => {
+        console.error('Error fetching HTML:', error);
+    });
+
