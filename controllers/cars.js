4898c124bd4ae97198d592d77af92df9088d08ac
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2024-05-08 17:52:08 +0100

    Get all cars endpoint

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,11 +1,21 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-exports.postCars = async (req, res) => {
-    //get car properties from the request
-    const { make, model, url} = req.body;
+//retreives all cars from db
+exports.getAll = async (req, res) => {
     try {
-        //creates a new car
+        const cars = await prisma.cars.findMany();
+        res.status(200).json(cars);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+}
+
+//add a car to the database
+exports.postCars = async (req, res) => {
+    const { make, model, url} = req.body;
+
+    try {
         const car = await prisma.cars.create({
             data: {
                 model: model,
@@ -13,9 +23,10 @@ exports.postCars = async (req, res) => {
                 url: url
             },
         })
-        //returns created car
+
         res.status(201).json(car)
     } catch (error) {
         res.status(400).json({ msg: error.message })
     }
 }
+
--- routes/cars.js
+++ routes/cars.js
@@ -1,7 +1,7 @@
 const route = require('express').Router();
 const controller = require('../controllers/cars')
 
-
+route.get('/get-all-cars', controller.getAll); //Retreives all cars
 route.post('/add-cars', controller.postCars); //Create new car
 
 module.exports = route;
