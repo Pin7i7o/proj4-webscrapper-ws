1ef35c46e4a66b5be80b92d7d9cfa82ea4b6ed66
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2024-06-17 18:03:31 +0100

    Added middleware to all routes

--- routes/responses.js
+++ routes/responses.js
@@ -1,5 +1,8 @@
 const route = require('express').Router();
 const controller = require('../controllers/responses');
+const authMiddleware = require('../middlewares/auth');
+
+route.use(authMiddleware);
 
 route.get('/get-id/:scrapId', controller.getById); //Responses for specific car
 route.post('/create', controller.create); //Create new responses
--- routes/scrap.js
+++ routes/scrap.js
@@ -1,5 +1,8 @@
 const controller = require('../controllers/scrap');
 const route = require('express').Router();
+const authMiddleware = require('../middlewares/auth');
+
+route.use(authMiddleware);
 
 route.get('/last-10-cars', controller.getLatestCars); //10 most recently posted cars route
 route.get('/get-id/:carId', controller.getById); //Scrap History for specific car
