f278b5370bcbe07c0ea4c65fe3881c9744902206
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2024-07-02 11:23:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+  });
+
 app.use('/api/', router);
 
 const port = process.env.SERVER_PORT || 8080;
